bcfa4a1919eeff0afdd0a72099e6cf42131cc1da
rozran00@gmail.com <rozran00@gmail.com>
2022-01-18 11:04:51 -0500

    adding files from today's class

---
+++ Released to Students/Homework/homework1/homework1_answers.R
@@ -0,0 +1,33 @@
+# Answer Key (YOUR NAME GOES HERE)
+# STAT 4380 - Data Science
+# Due: January 18, 2022
+
+# Problem 3
+
+# A 
+sqrt(100489)
+
+# B 
+# JAKE has 2 vowels and 2 consonants 
+2 / 2
+
+# C
+pi == (22 / 7)
+
+# D
+sqrt(150) > 12
+sqrt(150) < 13
+
+(sqrt(150) > 12) & (sqrt(150) < 13)
+
+# E 
+install.packages("gapminder")
+library(gapminder)
+
+# F
+head(gapminder)
+names(gapminder)
+str(gapminder)
+
+# G
+tail(gapminder, n = 10)
